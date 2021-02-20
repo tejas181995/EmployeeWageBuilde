e74b4a1097b0907d98a2573e4b82dbfb7747222e
tejas181995 <tejasdev92@gmail.com>
2021-02-20 17:46:51 +0530

    updated with monthly Wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,31 @@
 public class EmpWageBuilder{
     public static void main(String []args){
         System.out.println("Welcome to Employee Wage");
+        int FULL_TIME_HRS = 8;
+        int PART_TIME_HRS = 4;
+        int WAGE_PER_HR = 20;
+        int DAYS_OF_MONTH = 20;
+        int MONTHLY_WAGE;
+        int DAILY_WAGE;
+        int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+        
+        switch(empCheck) {
+            case 1:
+                System.out.println("Employee is full time");
+                DAILY_WAGE = FULL_TIME_HRS * WAGE_PER_HR;
+                MONTHLY_WAGE = DAILY_WAGE * DAYS_OF_MONTH;
+                System.out.println("Monthly wage is: " + MONTHLY_WAGE);
+                break;
+            case 2:
+                System.out.println("Empoyee is part time");
+                DAILY_WAGE = PART_TIME_HRS * WAGE_PER_HR;
+                MONTHLY_WAGE = DAILY_WAGE * DAYS_OF_MONTH;
+                System.out.println("Monthly wage is: " + MONTHLY_WAGE);
+                break;
+            default:
+                System.out.println("Employee is absent");
+                break;
+        }
+
     }
 }
