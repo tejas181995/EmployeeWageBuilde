e7e01e2c6a1cbb8336391a52623fe4a2df7bf977
tejas181995 <tejasdev92@gmail.com>
2021-03-02 15:11:08 +0530

    arraylist collection is used

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,3 +1,5 @@
+kimport java.util.*;
+
 interface IComputeEmpWage {
 	public void addCompanyWage(String companyName, int empWagePerHr, int daysInMonth, int maxHrs);
 	public void computeWage();
@@ -36,30 +38,33 @@ public class EmpWage implements IComputeEmpWage{
 	public static final int IS_PART_TIME = 2;
 
 	private int numOfCompany = 0;
-	private CompanyEmpWage[] companyEmpWageArray;
+	private ArrayList<CompanyEmpWage> companyEmpWageList;
 
 	public EmpWage() {
-		companyEmpWageArray = new CompanyEmpWage[5];
+		companyEmpWageList = new ArrayList<>();
 	}
 
 	public void addCompanyWage(String companyName, int empWagePerHr, int daysInMonth, int maxHrs) {
-		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(companyName, empWagePerHr, daysInMonth, maxHrs);
-		numOfCompany++;
+		CompanyEmpWage companyEmpWage = new CompanyEmpWage(companyName, empWagePerHr, daysInMonth, maxHrs);
+		companyEmpWageList.add(companyEmpWage);
 	}
 
 	public void computeWage() {
-		for (int i=0; i<numOfCompany; i++){
-			companyEmpWageArray[i].setTotalEmpWage(this.computeWage(companyEmpWageArray[i]));
-			System.out.println(companyEmpWageArray[i]);
+		for (int i=0; i<companyEmpWageList.size(); i++){
+			CompanyEmpWage companyEmpWage = companyEmpWageList.get(i);
+			companyEmpWage.setTotalEmpWage(this.computeWage(companyEmpWage));
+			System.out.println(companyEmpWage);
 		}
 	}
 
 	public int computeWage(CompanyEmpWage companyEmpWage){
 
+	
 		int totalWorkingDays = 0;
 		int totalEmpHrs = 0;
 		int empHrs = 0;
 		int empWage = 0;
+
 		while ( totalWorkingDays < companyEmpWage.daysInMonth && totalEmpHrs <= companyEmpWage.maxHrs ) {
 
 			int empCheck =(int) Math.floor(Math.random() * 10) % 3;
